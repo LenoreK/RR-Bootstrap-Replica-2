b7d0f5c2a6cc4a635533efdc4825edd3b9161289
Lenore Kelly <lenorekelly2@gmail.com>
2022-04-02 18:15:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Background from './components/Background';
+import StoreListings from './components/StoreListings';
+import Topbar from './components/Topbar.js';
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <Background />
+      <StoreListings />
+    </div>
+  )
+}
+
+export default App;
